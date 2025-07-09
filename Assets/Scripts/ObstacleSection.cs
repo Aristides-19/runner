437e13be2f8621c8e5c30819aa2b97856306929c
KarelinGuzman <>
2025-07-09 15:46:32 -0400

    ahora si cine

--- Assets/Scripts/ObstacleSection.cs
+++ Assets/Scripts/ObstacleSection.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sections : MonoBehaviour{
+public class Sections : MonoBehaviour {
+
     private int sectionsCount = 0;
-    public List<GameObject> obstacles;
-    
+    private int currentObstacleIndex = 0;
+
+    [SerializeField]
+    private List<GameObject> obstacles;
+    [SerializeField]
     private float speed = 10;
-    public float sectionSize = 0;
+    [SerializeField]
+    private float sectionSize = 48;
+
     void Start(){
         sectionsCount = GameObject.FindGameObjectsWithTag("Section").Length;
-        Debug.Log(sectionsCount);
         obstacles = new List<GameObject>();
 
         foreach (Transform child in transform.GetComponentsInChildren<Transform>(true))
@@ -21,26 +26,23 @@ public class Sections : MonoBehaviour{
             }
         }
 
-        Debug.Log(obstacles.Count);
-
-        EnableRamdomObstacle();
+        randomObstacle();
     }
+    public void randomObstacle() {
 
-    public void EnableRamdomObstacle() { 
-        foreach (GameObject obstacle in obstacles){
-            obstacle.SetActive(false);
-        }
+        obstacles[currentObstacleIndex].SetActive(false);
 
-        int ramdomIndex = Random.Range(0, obstacles.Count);
-        obstacles[ramdomIndex].SetActive(true);
+        currentObstacleIndex = Random.Range(0, obstacles.Count);
+        obstacles[currentObstacleIndex].SetActive(true);
 
     }
 
     void Update(){
         transform.Translate(Vector3.back * speed * Time.deltaTime);
 
-        if(transform.position.z <= -sectionSize)
+        if (transform.localPosition.z <= -sectionSize)
         {
+            randomObstacle();
             transform.Translate(Vector3.forward * sectionSize * sectionsCount);
         }
     }
